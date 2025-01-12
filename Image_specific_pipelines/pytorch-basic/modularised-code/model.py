431992e92c1ff3e1a13fe2b13173c90b5508fc4a
Ravikiran Bhonagiri <bhonagiri.ravikiran@gmail.com>
2025-01-12 13:55:56 -0600

    Create model.py

---
+++ Image_specific_pipelines/pytorch-basic/modularised-code/model.py
@@ -0,0 +1,19 @@
+# model.py
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DeepNN(nn.Module):
+    def __init__(self, input_size, num_classes):
+        super(DeepNN, self).__init__()
+        self.fc1 = nn.Linear(input_size, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        return x
