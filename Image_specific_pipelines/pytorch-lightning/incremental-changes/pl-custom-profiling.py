f4d19e7987e6ff7aa588eb3b6eb6a4f3e86b0743
Ravikiran Bhonagiri <bhonagiri.ravikiran@gmail.com>
2025-01-12 13:41:09 -0600

    Create pl-custom-profiling.py

---
+++ Image_specific_pipelines/pytorch-lightning/incremental-changes/pl-custom-profiling.py
@@ -0,0 +1,190 @@
+import torch
+import torch.nn.functional as F
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+import pytorch_lightning as pl
+import torchmetrics
+from torchmetrics import Metric
+from pytorch_lightning.callbacks import EarlyStopping, Callback
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.profilers import PyTorchProfiler
+
+torch.set_float32_matmul_precision("medium") # to make lightning happy
+
+
+
+class MyPrintingCallback(Callback):
+    def __init__(self):
+        super().__init__()
+
+    def on_train_start(self, trainer, pl_module):
+        print("Starting to train!")
+
+    def on_train_end(self, trainer, pl_module):
+        print("Training is done.")
+
+
+# Define a custom metric
+class MyAccuracy(Metric):
+    def __init__(self):
+        super().__init__()
+        self.add_state("correct", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("total", default=torch.tensor(0), dist_reduce_fx="sum")
+
+    def update(self, preds, target):
+        preds = torch.argmax(preds, dim=1)
+        assert preds.shape == target.shape
+        self.correct += torch.sum(preds == target)
+        self.total += target.numel()
+
+    def compute(self):
+        return self.correct.float() / self.total.float()
+
+
+# Define the model
+class NN(pl.LightningModule):
+    def __init__(self, input_size, num_classes):
+        super().__init__()
+        self.fc1 = nn.Linear(input_size, 50)
+        self.fc2 = nn.Linear(50, num_classes)
+        self.loss_fn = nn.CrossEntropyLoss()
+        self.accuracy = torchmetrics.Accuracy(task="multiclass", num_classes=num_classes)
+        self.my_accuracy = MyAccuracy()
+        self.f1_score = torchmetrics.F1Score(task="multiclass", num_classes=num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+    def _common_step(self, batch, batch_idx, stage):
+        x, y = batch
+        x = x.view(x.size(0), -1)  # Flatten the input
+        scores = self.forward(x)
+        loss = self.loss_fn(scores, y)
+        accuracy = self.my_accuracy(scores, y)
+        f1_score = self.f1_score(scores, y)
+        self.log_dict(
+            {
+                f"{stage}_loss": loss,
+                f"{stage}_accuracy": accuracy,
+                f"{stage}_f1_score": f1_score,
+            },
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+        )
+        return loss
+
+    def training_step(self, batch, batch_idx):
+        return self._common_step(batch, batch_idx, "train")
+
+    def validation_step(self, batch, batch_idx):
+        self._common_step(batch, batch_idx, "val")
+
+    def test_step(self, batch, batch_idx):
+        self._common_step(batch, batch_idx, "test")
+
+    def configure_optimizers(self):
+        return optim.Adam(self.parameters(), lr=0.001)
+
+
+# Define the Lightning Data Module
+class MnistDataModule(pl.LightningDataModule):
+    def __init__(self, data_dir, batch_size, num_workers):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+    def prepare_data(self):
+        datasets.MNIST(self.data_dir, train=True, download=True)
+        datasets.MNIST(self.data_dir, train=False, download=True)
+
+    def setup(self, stage=None):
+        entire_dataset = datasets.MNIST(
+            root=self.data_dir,
+            train=True,
+            transform=transforms.ToTensor(),
+            download=False,
+        )
+        self.train_ds, self.val_ds = random_split(entire_dataset, [50000, 10000])
+        self.test_ds = datasets.MNIST(
+            root=self.data_dir,
+            train=False,
+            transform=transforms.ToTensor(),
+            download=False,
+        )
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_ds,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=True,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_ds,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=False,
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_ds,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=False,
+        )
+
+
+# Hyperparameters
+input_size = 784  # 28x28 pixels
+num_classes = 10  # Digits 0-9
+batch_size = 64
+num_epochs = 3
+data_dir = "dataset/"
+num_workers = 4
+learning_rate = 0.001
+precision = 16
+
+# Initialize the data module and model
+dm = MnistDataModule(data_dir=data_dir, batch_size=batch_size, num_workers=num_workers)
+model = NN(input_size=input_size, num_classes=num_classes)
+
+if __name__ == "__main__":
+    logger = TensorBoardLogger("tb_logs", name="mnist_model_v0")
+    profiler = PyTorchProfiler(
+        on_trace_ready=torch.profiler.tensorboard_trace_handler("tb_logs/profiler0"),
+        schedule=torch.profiler.schedule(skip_first=10, wait=1, warmup=1, active=20),
+    )
+    model = NN(
+        input_size=input_size,
+        learning_rate=learning_rate,
+        num_classes=num_classes,
+    )
+    dm = MnistDataModule(
+        data_dir=data_dir,
+        batch_size=batch_size,
+        num_workers=num_workers,
+    )
+    trainer = pl.Trainer(
+        profiler=profiler,
+        logger=logger,
+        accelerator="gpu",
+        devices=[0],
+        min_epochs=1,
+        max_epochs=num_epochs,
+        precision=precision,
+        callbacks=[MyPrintingCallback(), EarlyStopping(monitor="val_loss")],
+    )
+    
+    trainer.fit(model, dm)
+    trainer.validate(model, dm)
+    trainer.test(model, dm)
